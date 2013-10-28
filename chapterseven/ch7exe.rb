18ec91b9afa266bbdb069f1055ce56ed0a31e26a
Zarni Htet <zhtet13@cornellcollege.edu>
2013-10-27 22:14:53 -0500

    Done with chapter 7

---
+++ chapterseven/ch7exe.rb
@@ -0,0 +1,66 @@
+#Exercise 1
+
+num = 99
+while (num >= 0)
+	num2 = num -1
+	if (num == 0)
+		puts 'No more bottles of beer on the wall, no more botles of beer.'
+		puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+	else
+		puts num.to_s + ' bottles of beer on the wall, ' + num.to_s + ' bottles of beer. '
+		puts 'Go to the store and buy some more, ' + num2.to_s + ' bottles of beer on the wall.'
+	end
+	num = num -1
+end
+ 
+ 
+# Exercise 2
+stat = ''
+puts 'Starts dialogue with \'deaf\' grandma!'
+while stat != 'BYE'
+	stat = gets.chomp
+	mrand = rand(1930..1950) 
+	if (stat == stat.upcase)
+		puts 'NO, NOT SINCE ' + mrand.to_s + '!'
+	else
+		puts 'HUH?! SPEAK UP, SONNY!'
+	end
+end
+
+# Exercise 3
+stat = ''
+puts 'Starts dialogue with \'deaf\' grandma!'
+count = 0
+while (count != 3)
+	stat = gets.chomp
+	if (stat == 'BYE')
+		count = count + 1
+	else
+		count = 0
+	end
+	mrand = rand(1930..1950) 
+	if (stat == stat.upcase)
+		puts 'NO, NOT SINCE ' + mrand.to_s + '!'
+	else
+		puts 'HUH?! SPEAK UP, SONNY!'
+	end
+end
+
+#Exercise 4
+puts 'Type in the starting year'
+syear = gets.chomp.to_i
+puts 'Type in the ending year'
+eyear = gets.chomp.to_i
+lyear = syear
+while (lyear<= eyear)
+	if (lyear%100 == 0)
+		if (lyear%400 == 0)
+			puts lyear
+		else		
+		end
+	elsif (lyear%4 ==0)
+		puts lyear
+	end
+	lyear = lyear + 1
+end	
+
--- chapterseven/ch7prac.rb
+++ chapterseven/ch7prac.rb
@@ -1,52 +1,100 @@
-#lexicographical ordering
-puts 'cat' < 'dog'
+# #lexicographical ordering
+# puts 'cat' < 'dog'
 
-#Capital letter before small letters
-puts 'bug lady' < 'Xander'
-puts 'bug lady'.downcase < 'Xander'.downcase
+# #Capital letter before small letters
+# puts 'bug lady' < 'Xander'
+# puts 'bug lady'.downcase < 'Xander'.downcase
 
-#Numbers in order from 0 and onwards
+# #Numbers in order from 0 and onwards
 
-puts  2 < 10
-puts '2' < '10'
-
-
-#Branching
+# puts  2 < 10
+# puts '2' < '10'
 
 
-puts 'Hello, what\'s your name?'
-name = gets.chomp
-puts 'Hello, ' + name + '.'
+# #Branching
 
-if name == 'Zarni'
-	puts 'What a lovely name!'
-end
 
-puts 'I am a fortune-teller. Tell me your name:'
-name = gets.chomp
+# puts 'Hello, what\'s your name?'
+# name = gets.chomp
+# puts 'Hello, ' + name + '.'
 
-if name == 'Chris'
-	puts 'I see great things in your future'
-else
-	puts 'Your future is...oh my! Look at the time!'
-	puts 'I really have to go, sorry!'
-end
+# if name == 'Zarni'
+# 	puts 'What a lovely name!'
+# end
 
-English teacher example
+# puts 'I am a fortune-teller. Tell me your name:'
+# name = gets.chomp
 
-puts 'Hello, and welcome to seventh grade English'
-puts 'My name is Mrs. Gabbard. And your name is ...?'
-name = gets.chomp
+# if name == 'Chris'
+# 	puts 'I see great things in your future'
+# else
+# 	puts 'Your future is...oh my! Look at the time!'
+# 	puts 'I really have to go, sorry!'
+# end
 
-if name  == name.capitalize
-	puts 'Please take a seat, ' + name + '.'
-else 
-	puts name + '? You mean ' + name.capitalize + ', right?'
-	puts 'Don\'t you even know how to spell your name??'
-	reply = gets.chomp
-	if reply.downcase == 'yes'
-		puts 'Please take a seat!'
-	else
-		puts 'Get the hell out of here!'
+# #English teacher example
+
+# puts 'Hello, and welcome to seventh grade English'
+# puts 'My name is Mrs. Gabbard. And your name is ...?'
+# name = gets.chomp
+
+# if name  == name.capitalize
+# 	puts 'Please take a seat, ' + name + '.'
+# else 
+# 	puts name + '? You mean ' + name.capitalize + ', right?'
+# 	puts 'Don\'t you even know how to spell your name??'
+# 	reply = gets.chomp
+# 	if reply.downcase == 'yes'
+# 		puts 'Please take a seat!'
+# 	else
+# 		puts 'Get the hell out of here!'
+# 	end
+# end
+
+
+# #Looping
+# input = ''
+# while input != 'bye'
+# 	puts input
+# 	input = gets.chomp
+# end
+# puts 'Come again soon!'
+
+# #Forever looping
+# while 'Spike' > 'Angel'
+# 	input = gets.chomp
+# 	puts input
+# 	if input == 'bye'
+# 		break
+# 	end
+# end
+# puts 'Come again soon!'
+
+
+# #A little bit of logic
+# puts 'Hello, what\'s your name?'
+# name = gets.chomp
+# puts 'Hello, ' + name + '.'
+# if name == 'Chris' || name == 'Katy'
+# 	puts 'What a lovely name!'
+
+
+#C's conversation with Chris
+while true
+	puts 'What would you like to ask C to do?'
+	request = gets.chomp
+	puts 'You say, "C, please ' + request + '"';
+
+	puts 'C \'s response:'
+	puts '"C ' + request + '."'
+	puts '"Papa ' + request + ', too."'
+	puts '"Mama ' + request + ', too."'
+	puts '"Ruby '  + request + ', too."'
+	puts '"Nono '  + request + ', too."'
+	puts '"Emma '  + request + ', too."'
+	puts
+	if request == 'stop'
+		break
 	end
 end
+
